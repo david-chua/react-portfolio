b7fc760f4ecd7d96fa69190ee86ab0655cba1615
david-chua <dp_chua@hotmail.com>
2019-06-21 18:24:30 -0400

    WIP

--- react-portfolio/src/Components/About.js
+++ react-portfolio/src/Components/About.js
@@ -1,119 +1,92 @@
 import React from 'react';
 import NavBar from './NavBar';
 import styled from 'styled-components';
+import smoke from './../Assets/Images/smoke_bg.jpg';
 
 const AboutContainer = styled.div`
   display: flex;
+  height: 100vh;
   flex-direction: row-reverse;
+  align-items: center;
+  background: url(${smoke});
+  background-repeat: no-repeat;
+  background-size: 50% 100%
 
   h1{
     text-align: center;
+  }
+
+  @media(max-width: 1000px){
+    flex-direction: column;
+    background: none;
   }
 `;
 
 const ImageContainer = styled.div`
   width: 50%;
-  padding-top: 3%;
   height: 100%;
+  padding-top: 3%;
+
+
+  @media(max-width: 1000px){
+    width: 100%;
+  }
 `;
 
 const AboutImg = styled.img`
-  width: 80%;
+  width: 90%;
   align-items: center;
 `;
 
+const AboutInfoDiv = styled.div`
+  width: 50%;
+  font-family: "Gill Sans MT";
+  color: #F5F4E9;
 
-const Curiosity = styled.div`
-  padding: 3% 5% 3% 0;
-  & p{
-    font-size: 1.2em;
-    font-weight: none !important;
-    padding-left: 10% !important;
-    text-align: left !important;
+  @media(max-width: 1000px){
+    width: 90%;
+    color: black;
   }
-
-  & p:nth-of-type(1){
-    padding-left: 10% !important;
-    text-align: left;
-  }
-
-  & p:nth-of-type(2){
-    padding-left: 10%;
-    text-align: left !important;
-  }
-
-& p:nth-of-type(3){
-  padding-left: 10% !important;
-  text-align: left;
-}
 `;
+
 
 const AboutInfo = styled.div`
   width: 50%;
+  font-family: "Gill Sans MT";
+  margin: 0 auto;
 
   & h1{
     text-align: center;
+
+    & span{
+      /* color: #063A7C; */
+      color: #80A4ED;
+      font-size: 1.5em;
+    }
   }
 
-  & h5{
-    font-size: 1.4em;
-    font-weight: bold;
+  & h3{
+    text-align: center;
+    & span{
+      color: #80A4ED;
+      font-size: 1.3em;
+    }
   }
 
-  & h5:nth-of-type(1){
-    padding-left: 10%;
-    padding-top: 15px;
-    padding-bottom: 15px;
-    text-align: left;
-    background-image: linear-gradient(to left, rgba(255,0,0,0), rgba(255,0,0,1));
-  }
-
-  & h5:nth-of-type(2){
-    padding-left: 0%;
-  }
-
-  & h5:nth-of-type(3){
-    padding-left: 70%;
+  @media(max-width: 1000px){
+    width: 100%;
   }
 `;
+
 
 
 class About extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      curiosity: true,
-      passionate: false,
-      competitive: false
     }
   }
 
-  openCuriosity = e => {
-    e.preventDefault();
-    this.setState({
-        curiosity: true,
-        passionate: false,
-        competitive: false
-    })
-  }
-
-  openPassion = e => {
-    e.preventDefault();
-    this.setState({
-        curiosity: false,
-        passionate: true,
-        competitive: false
-    })
-  }
-
-  openCompetition = e => {
-    e.preventDefault();
-    this.setState({
-        curiosity: false,
-        passionate: false,
-        competitive: true
-    })
-  }
 
 
   render(){
@@ -124,34 +97,15 @@ class About extends React.Component{
           <ImageContainer>
             <AboutImg src={require("./../Assets/Images/aboutme.png")} />
           </ImageContainer>
-          <AboutInfo>
-            <h1> About Me </h1>
-            <h5 onClick={this.openCuriosity}>Curious</h5>
-            <h5 onClick={this.openPassion}>Passionate</h5>
-            <h5 onClick={this.openCompetition}>Competitive</h5>
-
-            {this.state.curiosity &&
-              <Curiosity className="curiosity">
-                <p> I'm just a curious guy who loves to learn. </p>
-                <p> Not only did my educational background stem from my curiousity, it also is the reason for why I'm in the programming world. </p>
-                <p> Being a biology major, programming was something I never really thought about until I became a technical recruiter. It opened my world into learning the different aspects of technology.</p>
-                <p> And from there, the learning never stops. And I'm happy with that. </p>
-
-
-
-              </Curiosity>
-            }
-            {this.state.passionate &&
-              <div>
-                Passion
-              </div>
-            }
-            {this.state.competitive &&
-              <div>
-                Competition
-              </div>
-            }
-          </AboutInfo>
+          <AboutInfoDiv>
+            <AboutInfo>
+              <h1> Hi, my name is <span> David Chua </span> </h1>
+              <h2> I'm a full stack software developer with a love for all things React </h2>
+              <h2> My passion with programming comes from idea that I can create anything I want with just the letters in my keyboard</h2>
+              <h3> <span> Games</span> maybe, <span>Web Apps</span> or even...</h3>
+              <h3><span>...Voice apps</span> are all within my fingertips, you just have to start typing</h3>
+            </AboutInfo>
+          </AboutInfoDiv>
         </AboutContainer>
       </div>
     )
